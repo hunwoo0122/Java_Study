8a87e4c91c1f4141f6d0973e7f8d4cbac1f556cd
ParkHeonWoo <phwoo1315@naver.com>
2024-10-17 12:18:00 +0900

    자바 공부 기록

---
+++ src/main/java/org/example/day12/Inherit/MultiThread.java
@@ -0,0 +1,11 @@
+package org.example.day12.Inherit;
+
+public class MultiThread {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start();
+        t2.start();
+    }
+}
